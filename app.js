3a181d9cb1ee95e91e70d380bebcb3a9b3bf9eab
Henrik Karlsson <hk.henrik@gmail.com>
2016-10-22 20:41:29 +0200

    Eslint support

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const Koa = require('koa');
+const Koa = require('koa')
 
-const app = new Koa();
+const app = new Koa()
 
 app.use(async (ctx, next) => {
   ctx.body = 'test'
-});
+})
 
 app.listen(3000)
