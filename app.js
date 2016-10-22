1dbb5f2d7cba0c3bcfa2ce2c2ae00035c0fe94e9
Henrik Karlsson <hk.henrik@gmail.com>
2016-10-22 20:50:39 +0200

    Routing

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const Koa = require('koa')
+const Router = require('koa-router')
+const BodyParser = require('koa-bodyparser')
 
 const app = new Koa()
 
-app.use(async (ctx, next) => {
-  ctx.body = 'test'
+const bots = []
+
+const router = new Router()
+
+router.get('/status', async (ctx, next) => {
+  ctx.body = {
+    bots: bots.length
+  }
 })
+
+router.get('/add-bot', async (ctx, next) => {
+  bots.push({})
+})
+
+app
+  .use(BodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000)
